d8f166314530193609bae5c5dd8bf08f878d930f
Lennart Hoyer <lennart.hoyer@gmail.com>
2021-03-23 11:20:36 +0100

    Redigert connection url

--- src/DBConn.java
+++ src/DBConn.java
@@ -1,7 +1,9 @@
 import java.sql.*;
 import java.util.Properties;
 
-public abstract class DBConn {
+import javax.print.event.PrintEvent;
+
+public class DBConn {
   protected Connection conn;
   public DBConn () {
   }
@@ -9,12 +11,16 @@ public abstract class DBConn {
     try {
           // Class.forName("com.mysql.jdbc.Driver").newInstance(); when you are using MySQL 5.7
     Class.forName("com.mysql.cj.jdbc.Driver"); 
-          // Properties for user and password.
-          Properties p = new Properties();
-          p.put("user", "myuser");
-          p.put("password", "mypassword");           
-    //            conn = DriverManager.getConnection("jdbc:mysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
-          conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/ekt?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false",p);
+
+            // Properties for user and password.
+            Properties p = new Properties();
+            p.put("user", "lenhoy");
+            p.put("password", "");           
+
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/DatDat_DB?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false",p);
+     
+            System.out.println("Hello world 2!");
+
       } catch (Exception e)
     {
           throw new RuntimeException("Unable to connect", e);
