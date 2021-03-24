0753227443de7ea93bc5997747b948c70fb2f883
Lars Moen <larmoe@stud.ntnu.no>
2021-03-24 12:32:24 +0100

    flere eksempler

--- src/LoginCtrl.java
+++ src/LoginCtrl.java
@@ -1,3 +1,20 @@
+import java.sql.*;
+import java.util.*;
+
 public class LoginCtrl extends DBConn{
-    
+    private String email; 
+    private String name; 
+    private String password; 
+    private String anonAlias; 
+    private PreparedStatement loginStatement;
+
+    public void Login(String email, String password) {
+        this.email = email; 
+        this.password = password; 
+        try {
+            loginStatement = conn.prepareStatement("SELECT * FROM User");
+        } catch (Exception e) {
+            System.out.println("DB error during prepare of select from User" + e);
+        }
+    }
 }
