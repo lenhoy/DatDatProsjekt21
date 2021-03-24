45bfcb085c0a066da262a7bb8778fe44797d8540
Lennart Hoyer <lennart.hoyer@gmail.com>
2021-03-24 16:26:10 +0100

    Lagt til .connect i main

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         //TODO app logic here 
-        LoginCtrl login1 = new LoginCtrl(); 
-        login1.login("a@b.c", "123");
-        PostCtrl Session = new PostCtrl(1);
+        // LoginCtrl login1 = new LoginCtrl(); 
+        // login1.connect();
+        // login1.login("a@b.c", "123");
+        PostCtrl postSession = new PostCtrl("a@b.c");
+        postSession.connect();
+        postSession.postAThread(4145, "General", "Min første Thread", "Tag1", "Min første Header");
 
-        // Session.startThread();
-
+        
     }
 }
